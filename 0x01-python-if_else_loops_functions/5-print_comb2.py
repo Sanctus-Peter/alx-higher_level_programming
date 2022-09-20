6edb0e8a0f55ef25ed11ce43db0ed71d6f79ccc6
Sanctus-Peter <sanctuspeter@gmail.com>
2022-09-21 00:44:11 +0100

    mandatory and advance tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(100):
-    print(f"{:02d}".format(num), end='\n' if num == 99 else '')
+    print("{:02d}".format(num), end='\n' if num == 99 else '')
     if num < 99:
         print(", ", end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for num in range(9):
     for var in range(num + 1, 10):
-        print(f"{num:d}{var:d}", end='\n' if num == 8 else ', ')
+        print("{:d}{:d}".format(num, var), end='\n' if num == 8 else ', ')
