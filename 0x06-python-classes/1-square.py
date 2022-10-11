c2de0ed23d90a9e0826340371e3725294fba458c
Sanctus-Peter <sanctuspeter@gmail.com>
2022-10-11 22:43:16 +0100

    python class task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Defines a Square and its basic properties
+
+    >>> square_1 = Square()
+    >>> square_2 = Square(7)
+    """
+
+    def __init__(self, size: int) -> None:
+        """
+        Innitialize the size of the square
+
+        :param size: int size of square
+        """
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Defines a Square and its basic properties
+
+    >>> square_1 = Square()
+    >>> square_2 = Square(7)
+    """
+
+    def __init__(self, size=0) -> None:
+        """
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
+
+        :param size: int size of square ( > 0)
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self) -> int:
+        """
+        Calculates and returns the area of the square
+
+        :return: the area of the square
+        """
+
+        return self.__size ** 2
