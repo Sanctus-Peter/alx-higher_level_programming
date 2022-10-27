4e43f56af92818895c0b1ccccd1193490c78021f
Sanctus-Peter <sanctuspeter@gmail.com>
2022-10-27 09:28:34 +0100

    pycodestyle fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 """Reads from standard input and computes metrics.
-
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
@@ -11,7 +9,6 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
-
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -19,6 +16,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
